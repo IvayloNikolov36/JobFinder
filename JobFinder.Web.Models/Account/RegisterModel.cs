d246361cb85204555c7c8f4fa9cea965603c4244
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-03-29 19:05:27 +0200

    changes in register user - username is removed

--- JobFinder.Web.Models/Account/RegisterModel.cs
+++ JobFinder.Web.Models/Account/RegisterModel.cs
@@ -1,57 +1,48 @@
 ﻿namespace JobFinder.Web.Models.Account
 {
+    using AutoMapper;
     using JobFinder.Data.Models;
     using JobFinder.Services.Mappings;
     using System.ComponentModel.DataAnnotations;
 
-    public class RegisterModel : IMapTo<UserEntity>
+    public class RegisterModel : IHaveCustomMappings
     {
-        [Required]
-        [StringLength(
-            25,
-            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-            MinimumLength = 6)]
-        public string UserName { get; set; }
+        private const int PasswordMinLength = 6;
+        private const int PasswordMaxLength = 35;
+        private const int MinNameLength = 2;
+        private const int MaxNameLength = 25;
+        private const string ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.";
 
         [Required]
         [EmailAddress]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(
-            100,
-            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-            MinimumLength = 6)]
+        [StringLength(PasswordMaxLength, ErrorMessage = ErrorMessage, MinimumLength = PasswordMinLength)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Compare(
-            "Password",
-            ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare(nameof(this.Password), ErrorMessage = ErrorMessage)]
         public string ConfirmPassword { get; set; }
 
         [Required]
-        [StringLength(
-            20,
-            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-            MinimumLength = 2)]
+        [StringLength(MaxNameLength, ErrorMessage = ErrorMessage, MinimumLength = MinNameLength)]
         public string FirstName { get; set; }
 
         [Required]
-        [StringLength(
-            20, 
-            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
-            MinimumLength = 2)]
+        [StringLength(MaxNameLength, ErrorMessage = ErrorMessage, MinimumLength = MinNameLength)]
         public string MiddleName { get; set; }
 
         [Required]
-        [StringLength(
-            20, 
-            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
-            MinimumLength = 2)]
+        [StringLength(MaxNameLength, ErrorMessage = ErrorMessage, MinimumLength = MinNameLength)]
         public string LastName { get; set; }
-        
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<RegisterModel, UserEntity>()
+                .ForMember(e => e.UserName, o => o.MapFrom(vm => vm.Email));
+        }
     }
 }
