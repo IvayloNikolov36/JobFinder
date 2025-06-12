7c1597586099313dcc45ac2ed849d31be0da2e14
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-06-13 01:48:27 +0300

    new cities seed

---
+++ JobFinder.Data/Migrations/20250612224735_NewCitiesSeed.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace JobFinder.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewCitiesSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "CreatedOn", "ModifiedOn", "Name" },
+                values: new object[,]
+                {
+                    { 10, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Dobrich" },
+                    { 11, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Vraca" },
+                    { 12, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Sliven" },
+                    { 13, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Kazanlak" },
+                    { 14, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Vidin" },
+                    { 15, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Nova Zagora" },
+                    { 16, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Shumen" },
+                    { 17, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Silistra" },
+                    { 18, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Targovishte" },
+                    { 19, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Pazarjik" },
+                    { 20, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, "Yambol" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 20);
+        }
+    }
+}
--- JobFinder.Data/Seeders/CitiesSeeder.cs
+++ JobFinder.Data/Seeders/CitiesSeeder.cs
@@ -16,7 +16,18 @@ namespace JobFinder.Data.Seeders
                 new CityEntity { Name = "Stara Zagora", Id = 6 },
                 new CityEntity { Name = "Pleven", Id = 7 },
                 new CityEntity { Name = "Gabrovo", Id = 8 },
-                new CityEntity { Name = "Veliko Tarnovo", Id = 9 }
+                new CityEntity { Name = "Veliko Tarnovo", Id = 9 },
+                new CityEntity { Name = "Dobrich", Id = 10 },
+                new CityEntity { Name = "Vraca", Id = 11 },
+                new CityEntity { Name = "Sliven", Id = 12 },
+                new CityEntity { Name = "Kazanlak", Id = 13 },
+                new CityEntity { Name = "Vidin", Id = 14 },
+                new CityEntity { Name = "Nova Zagora", Id = 15 },
+                new CityEntity { Name = "Shumen", Id = 16 },
+                new CityEntity { Name = "Silistra", Id = 17 },
+                new CityEntity { Name = "Targovishte", Id = 18 },
+                new CityEntity { Name = "Pazarjik", Id = 19 },
+                new CityEntity { Name = "Yambol", Id = 20 }
             );
         }
     }
