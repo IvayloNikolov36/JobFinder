7216a619d52a24e6e3285e650277df0c8f370e8a
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-06-13 01:38:06 +0300

    fix mapping for workplaceTypeId

--- JobFinder.Data.Models/Extensions/AnonymousProfile/AnonymousProfileAppearanceEntity.cs
+++ JobFinder.Data.Models/Extensions/AnonymousProfile/AnonymousProfileAppearanceEntity.cs
@@ -10,7 +10,7 @@ public partial class AnonymousProfileAppearanceEntity : IHaveCustomMappings
     public void CreateMappings(IProfileExpression configuration)
     {
         configuration.CreateMap<AnonymousProfileAppearanceEntity, AnonymousProfileAppearanceDTO>()
-            .ForMember(dto => dto.WorkplaceTypes, o => o.MapFrom(e => e.WorkplaceTypes.Select(wpt => wpt.Id)))
+            .ForMember(dto => dto.WorkplaceTypes, o => o.MapFrom(e => e.WorkplaceTypes.Select(wpt => wpt.WorkplaceTypeId)))
             .ForMember(dto => dto.JobEngagements, o => o.MapFrom(e => e.JobEngagements.Select(je => je.JobEngagementId)))
             .ForMember(dto => dto.SoftSkills, o => o.MapFrom(e => e.SoftSkills.Select(ss => ss.SoftSkillId)))
             .ForMember(dto => dto.ITAreas, o => o.MapFrom(e => e.ITAreas.Select(a => a.ITAreaId)))
