eb700e58edf91440f1b2231d50a9c7c648d8680a
DESKTOP-3E10FSC\INikolov <ivailonikolov36@gmail.com>
2025-07-24 01:08:10 +0300

    company subscriptions distributed cache

--- JobFinder.Services/Constants/CacheConstants.cs
+++ JobFinder.Services/Constants/CacheConstants.cs
@@ -5,4 +5,5 @@ public class CacheConstants
     public static string CVsCacheKey = "cvs_{0}";
     public static string CvCacheKey = "cv_{0}";
     public static string JobSubscriptionsCacheKey = "jobSubscriptions_{0}";
+    public static string CompanySubscriptionsCacheKey = "companySubscriptions_{0}";
 }
--- JobFinder.Services/Implementations/CompanySubscriptionsService.cs
+++ JobFinder.Services/Implementations/CompanySubscriptionsService.cs
@@ -2,6 +2,10 @@
 using JobFinder.DataAccess.UnitOfWork;
 using JobFinder.Transfer.DTOs;
 using JobFinder.Web.Models.Subscriptions.CompanySubscriptions;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+using static JobFinder.Services.Constants.CacheConstants;
 
 namespace JobFinder.Services.Implementations
 {
@@ -9,47 +13,112 @@ namespace JobFinder.Services.Implementations
     {
         private readonly IEntityFrameworkUnitOfWork unitOfWork;
         private readonly IMapper mapper;
+        private readonly IDistributedCache distributedCache;
 
         public CompanySubscriptionsService(
             IEntityFrameworkUnitOfWork unitOfWork,
-            IMapper mapper)
+            IMapper mapper,
+            IDistributedCache distributedCache)
         {
             this.unitOfWork = unitOfWork;
             this.mapper = mapper;
+            this.distributedCache = distributedCache;
         }
 
         public async Task<IEnumerable<CompanyJobAdsForSubscribersViewModel>> GetLatesJobAds()
         {
-            IEnumerable<CompanyJobAdsForSubscribersDTO> jobAds = await this.unitOfWork.CompanySubscriptionsRepository
+            IEnumerable<CompanyJobAdsForSubscribersDTO> jobAds = await this.unitOfWork
+                .CompanySubscriptionsRepository
                 .GetCompanyAdsBySubscriptions();
 
-            return this.mapper.Map<IEnumerable<CompanyJobAdsForSubscribersViewModel>>(jobAds);
+            return this.mapper
+                .Map<IEnumerable<CompanyJobAdsForSubscribersViewModel>>(jobAds);
         }
 
         public async Task Subscribe(int companyId, string userId)
         {
-            await this.unitOfWork.CompanySubscriptionsRepository.Subscribe(userId, companyId);
+            await this.unitOfWork.CompanySubscriptionsRepository
+                .Subscribe(userId, companyId);
+
             await this.unitOfWork.SaveChanges();
+
+            await this.InvalidateCache(userId);
         }
 
         public async Task Unsubscribe(int companyId, string userId)
         {
-            await this.unitOfWork.CompanySubscriptionsRepository.Delete(userId, companyId);
+            await this.unitOfWork.CompanySubscriptionsRepository
+                .Delete(userId, companyId);
+
             await this.unitOfWork.SaveChanges();
+
+            await this.InvalidateCache(userId);
         }
 
         public async Task UnsubscribeAll(string userId)
         {
-            this.unitOfWork.CompanySubscriptionsRepository.DeleteSubscriptions(userId);
-            await this.unitOfWork.SaveChanges();          
+            this.unitOfWork.CompanySubscriptionsRepository
+                .DeleteSubscriptions(userId);
+
+            await this.unitOfWork.SaveChanges();
+
+            await this.InvalidateCache(userId);
         }
 
-        public async Task<IEnumerable<CompanySubscriptionViewModel>> GetMySubscriptions(string userId)
+        public async Task<IEnumerable<CompanySubscriptionViewModel>> GetMySubscriptions(
+            string userId)
         {
-            IEnumerable<CompanySubscriptionDTO> subscriptions = await this.unitOfWork.CompanySubscriptionsRepository
-                .GetMySubscriptions(userId);
+            string cacheKey = GetCompanySubscriptionCacheKey(userId);
 
-            return this.mapper.Map<IEnumerable<CompanySubscriptionViewModel>>(subscriptions);
+            byte[] subscriptionsCache = await this.distributedCache
+                .GetAsync(cacheKey);
+
+            if (subscriptionsCache == null)
+            {
+                IEnumerable<CompanySubscriptionDTO> subscriptions = await this.unitOfWork
+                    .CompanySubscriptionsRepository
+                    .GetMySubscriptions(userId);
+
+                var subscriptionModels = this.mapper
+                    .Map<IEnumerable<CompanySubscriptionViewModel>>(subscriptions);
+
+                await this.SetDataInCache(subscriptionModels, cacheKey);
+
+                return subscriptionModels;
+            }
+
+            return this.DeserializeData(subscriptionsCache);
+        }
+
+        private async Task SetDataInCache(
+            IEnumerable<CompanySubscriptionViewModel> data,
+            string cacheKey)
+        {
+            string serializedData = JsonSerializer.Serialize(data);
+
+            await this.distributedCache
+                .SetAsync(cacheKey, Encoding.UTF8.GetBytes(serializedData));
+        }
+
+        private async Task InvalidateCache(string userId)
+        {
+            string cacheKey = GetCompanySubscriptionCacheKey(userId);
+
+            await this.distributedCache.RemoveAsync(cacheKey);
+        }
+
+        private IEnumerable<CompanySubscriptionViewModel> DeserializeData(
+            byte[] subscriptionsData)
+        {
+            string dataString = Encoding.UTF8.GetString(subscriptionsData);
+
+            return JsonSerializer
+                .Deserialize<IEnumerable<CompanySubscriptionViewModel>>(dataString);
+        }
+
+        private static string GetCompanySubscriptionCacheKey(string userId)
+        {
+            return string.Format(CompanySubscriptionsCacheKey, userId);
         }
     }
 }
